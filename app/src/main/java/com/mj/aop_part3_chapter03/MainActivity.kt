e15d899df9506eb504360922941b468f4b6cd5e7
MJ <eoqkrskfk94@naver.com>
2021-05-18 00:12:18 +0900

    [Add] 알람 설정 화면 구현하기

--- app/src/main/java/com/mj/aop_part3_chapter03/MainActivity.kt
+++ app/src/main/java/com/mj/aop_part3_chapter03/MainActivity.kt
@@ -1,11 +1,76 @@
 package com.mj.aop_part3_chapter03
 
+import android.app.TimePickerDialog
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initOnOffButton()
+        initChangeAlarmTimeButton()
+
+
+
+    }
+
+    private fun initOnOffButton() {
+        val onOffButton = findViewById<Button>(R.id.onOffButton)
+        onOffButton.setOnClickListener {
+            //데이터를 확인한다
+
+            //온오프에 따라 작업을 처리한다
+
+            //오프 -> 알람을 제거
+            //온 -> 알람을 등록
+
+            //데이터 저장
+        }
+    }
+
+    private fun initChangeAlarmTimeButton() {
+        val changeAlarmButton = findViewById<Button>(R.id.changeAlarmTimeButton)
+        changeAlarmButton.setOnClickListener {
+
+            val calendar = Calendar.getInstance()
+
+            TimePickerDialog(this, { picker, hour , minute ->
+
+                val model = saveAlarmModel(hour, minute, false)
+
+
+            }, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE),false)
+                .show()
+
+        }
+    }
+
+    private fun saveAlarmModel(
+        hour: Int,
+        minute: Int,
+        onOff: Boolean
+    ): AlarmDisplayModel {
+        val model = AlarmDisplayModel(
+            hour = hour,
+            minute = minute,
+            onOff = onOff
+        )
+
+        val sharedPreferences = getSharedPreferences("time", Context.MODE_PRIVATE)
+
+        with(sharedPreferences.edit()) {
+            putString("alarm", model.makeDataForDB())
+            putBoolean("onOff", model.onOff)
+            commit()
+        }
+
+        return model
     }
 }
